c9b9ba85f7277d616accd1ba3a9720924c207fad
tahis9 <147927771+tahis9@users.noreply.github.com>
2025-03-05 18:52:01 +1100

    Complete requirements

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const docWidth = 960;
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 const opacityDelta = 0.1;
+const btn = document.querySelector(".btn");
+
 
 
 function createSquares(sideSquares) {
@@ -15,11 +17,28 @@ function createSquares(sideSquares) {
   let squares = document.querySelectorAll(".squares");
   squares.forEach((element,i) => { 
     element.addEventListener("mouseenter", () => {
-      squares[i].style.opacity = Number(squares[i].style.opacity)+opacityDelta; 
+      squares[i].style.opacity = Number(squares[i].style.opacity)+opacityDelta;
+      console.log(i) 
     });
   });
 }
 
-
+function reset() {
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+  let squaresChoice;
+  squaresChoice = +window.prompt("Squares per side for new canvas (Max: 100)", 16);
+  let i = 1;
+  while (i){
+    if (squaresChoice <= 100 && squaresChoice > 0 && Number.isInteger(squaresChoice)) {
+      createSquares(squaresChoice);
+      return
+    } else {
+      squaresChoice = +window.prompt("Invalid response. Please enter a positive integer less than 101", 16);
+    }
+  }
+}
 
 document.addEventListener("DOMContentLoaded", createSquares(16));
+btn.addEventListener("click", reset);
